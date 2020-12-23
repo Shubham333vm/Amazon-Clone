23002288d9c69daed39cd4e5c94d5415c87c84ac
Shubham333vm <s.shrivastava.ext@lafargeholcim.com>
2020-12-23 17:35:29 +0530

    first commit

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Logo() {
+    return (
+        <div>
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/62/Amazon.com-Logo.svg" alt="company logo"/>
+        </div>
+    )
+}
